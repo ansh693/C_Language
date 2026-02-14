76607d6fe9d3a4c75fddb75ec23df68efddb30a5
ansh693 <anshvjgupta@gmail.com>
2026-02-15 01:30:42 +0530

    Initial commit

---
+++ hello.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main(){
+    //1
+    //printf("hello world \n ");
+
+    //2
+    //float pi = 3.14 ;
+    //printf("the value of the pi is %f\n",pi);
+    
+    //3
+    //int number ;
+    //printf("enter the number you wnat to print :");
+    //scanf("%d", &number);
+    //printf("the number you have written is %d\n" , number);
+    
+    //4
+    //int a , b ;
+    //printf("Enter the a value :");
+    //scanf("%d", &a);
+    //printf("Enter the a value : ");
+    //scanf("%d",&b);
+    //int sum = a+b ;
+    //printf("The sum of a and b is %d" , sum);
+    
+    //5  write a program area of square 
+    //float side ;
+    //printf("Enter the side:");
+    //scanf("%f",&side);
+    //printf("area is : %f",side*side);
+
+    //calculate the area of the circle 
+    float radius ;
+    printf("Enter the radius:");
+    scanf("%f",&radius);
+    printf("The area of the circle is %d " , 3.14 * radius * radius);
+
+
+
+
+
+
+
+    return 0 ;
+}
